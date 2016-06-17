45bd7209a6a3dd16c4ed13c427f691ab92b9ac75
Igor Seleznov <seleznov.igor@gmail.com>
2016-06-17 15:55:31 -0700

    add the price counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
-function Pizza() {
-  this.base = base;
-  this.sause = sause;
-  this.toppings = [];
-}
-function Toppings () {
-  this.chicken = chicken;
-  this.olives = olives;
-  this.mozzarella = mozzarella;
-  this.champignons = champignons;
-}
-
-Pizza.prototype.myOrder = function() {
-
-}
+// function Pizza() {
+//   this.base = base;
+//   this.sause = sause;
+//   this.toppings = [];
+// }
+// function Toppings () {
+//   this.chicken = chicken;
+//   this.olives = olives;
+//   this.mozzarella = mozzarella;
+//   this.champignons = champignons;
+// }
+//
+// Pizza.prototype.myOrder = function() {
+//
+// }
 
 // user interface logic
-$("form#ingredients").submit(function(event) {
-  event.preventDefault();
-  var selected = [];
-  $('input:checked').each(function() {
-      selected.push($(this).attr('value'));
+$(document).ready(function(){
+  $("form#ingredients").submit(function(event) {
+    event.preventDefault();
+    var selected = [];
+    $('input:checked').each(function() {
+        selected.push($(this).attr('value'));
+    });
+    var price = 0;
+    for (var i in selected) {
+      price += parseInt(selected[i]);
+    }
+    $("#price").html(price);
   });
 });
